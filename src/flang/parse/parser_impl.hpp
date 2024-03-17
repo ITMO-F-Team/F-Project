553055fe1b2d346b7d5a00c6e1b9ec260271b587
achains <arthur.salio@gmail.com>
2024-03-17 16:43:55 +0300

    feat: parser impl base parsers

---
+++ src/flang/parse/parser_impl.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include "flang/parse/ast.hpp"
+#include "flang/tokenize/token.hpp"
+
+namespace flang {
+class ParserImpl {
+ public:
+  explicit ParserImpl(std::vector<Token> const& tokens) : tokens_(tokens), next_index_(0) {}
+
+  std::unique_ptr<ProgramNode> parseProgram();
+
+ private:
+  std::vector<Token> tokens_;
+  size_t next_index_;
+
+  // ----- Primitive parsers -----
+
+  bool atEOF() const;
+
+  std::optional<Token> peekOptional() const;
+
+  Token peekNext() const;
+
+  std::optional<Token> takeOptional();
+
+  Token takeNext();
+
+  Token eat(TokenType token_type);
+
+  // ----- Parsers for main non-terminals -----
+  /**
+  Element :=
+    | Identifier
+    | IntegerLiteral
+    | RealLiteral
+    | ListLikeElement
+  */
+  std::unique_ptr<ElementNode> parseElement();
+
+  /**
+  Identifier := tkIDENTIFIER.
+  */
+  std::unique_ptr<IdentifierNode> parseIdentifier();
+
+  /**
+  IntegerLiteral := tkINTEGER.
+  */
+  std::unique_ptr<IntegerLiteralNode> parseIntegerLiteral();
+
+  /**
+  RealLiteral := tkREAL.
+  */
+  std::unique_ptr<RealLiteralNode> parseRealLiteral();
+
+  /**
+  ListLikeElement := ( ListLikeElementBody ).
+  ListLikeElementBody :=
+    | Call
+    | Quote
+    | Setq
+    | Func
+    | Lambda
+    | Prog
+    | Cond
+    | While
+    | Return
+    | Break
+  */
+  std::unique_ptr<ElementNode> parseListLikeElement();
+};
+}  // namespace flang
