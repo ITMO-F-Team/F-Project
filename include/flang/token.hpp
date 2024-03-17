8f1a1e3324d6c09bd127286e0effd44b097f6694
achains <arthur.salio@gmail.com>
2024-03-17 21:46:35 +0300

    feat: add token location

--- include/flang/token.hpp
+++ include/flang/token.hpp
@@ -7,7 +7,12 @@
 namespace flang {
 class Token {
  public:
-  Token(TokenType type, std::string value);
+  struct TokenLocation {
+    size_t line;
+    size_t col;
+  };
+
+  Token(TokenType type, std::string value, size_t line, size_t col);
 
   std::string const& value() const;
 
@@ -15,8 +20,11 @@ class Token {
 
   TokenType type() const;
 
+  TokenLocation location() const;
+
  private:
   TokenType type_;
   std::string value_;
+  TokenLocation location_;
 };
 }  // namespace flang
--- src/flang/token.cpp
+++ src/flang/token.cpp
@@ -1,11 +1,14 @@
 #include "flang/token.hpp"
 
 namespace flang {
-Token::Token(TokenType type, std::string value) : type_(type), value_(std::move(value)) {}
+Token::Token(TokenType type, std::string value, size_t line, size_t col)
+    : type_(type), value_(std::move(value)), location_({.line = line, .col = col}) {}
 
 std::string const& Token::value() const { return value_; }
 
 std::string Token::value() { return value_; }
 
 TokenType Token::type() const { return type_; }
+
+Token::TokenLocation Token::location() const { return location_; }
 }  // namespace flang
