6d80e0734f8d17e71f8cf8a5998db1b1d579f2aa
a.salyu <arthur.salio@gmail.com>
2024-05-14 19:12:02 +0300

    feat: parser

--- src/flang/parse/parser_impl.cpp
+++ src/flang/parse/parser_impl.cpp
@@ -1,130 +1,152 @@
 #include "parser_impl.hpp"
 
-#include <flang/parse/ast.hpp>
-#include <flang/tokenize/token_type.hpp>
 #include <memory>
 #include <vector>
 
 #include "flang/flang_exception.hpp"
-#include "flang/pp/print_token.hpp"
+#include "flang/parse/ast.hpp"
+#include "flang/tokenize/token_type.hpp"
 
-namespace flang {
-bool ParserImpl::atEOF() const { return next_index_ >= tokens_.size(); }
-
-std::optional<Token> ParserImpl::peekOptional() const {
-  return (atEOF() ? std::optional<Token>() : tokens_[next_index_]);
+namespace flang
+{
+bool ParserImpl::atEOF() const
+{
+    return next_index_ >= tokens_.size();
 }
 
-Token ParserImpl::peekNext() const {
-  auto token = peekOptional();
-  if (!token.has_value()) {
-    throw parser_exception("Unexpected EOF");
-  }
-  return token.value();
+std::optional<Token> ParserImpl::peekOptional() const
+{
+    return (atEOF() ? std::optional<Token>() : tokens_[next_index_]);
 }
 
-std::optional<Token> ParserImpl::takeOptional() { return (atEOF() ? std::optional<Token>() : tokens_[next_index_++]); }
-
-Token ParserImpl::takeNext() {
-  auto token = takeOptional();
-  if (!token.has_value()) {
-    throw parser_exception("Unexpected EOF");
-  }
-  return token.value();
+Token ParserImpl::peekNext() const
+{
+    auto token = peekOptional();
+    if (!token.has_value()) {
+        throw parser_exception("Unexpected EOF");
+    }
+    return token.value();
 }
 
-std::unique_ptr<ProgramNode> ParserImpl::parseProgram() {
-  std::vector<std::unique_ptr<ElementNode>> elements;
-  while (!atEOF()) {
-    elements.emplace_back(parseElement());
-  }
-
-  return std::make_unique<ProgramNode>(std::move(elements));
+std::optional<Token> ParserImpl::takeOptional()
+{
+    return (atEOF() ? std::optional<Token>() : tokens_[next_index_++]);
 }
 
-Token ParserImpl::eat(TokenType token_type) {
-  auto token = takeNext();
-  if (token.type() != token_type) {
-    throw parser_exception(std::string("Unexpected token (eat): ") + token.value());
-  }
-  return token;
+Token ParserImpl::takeNext()
+{
+    auto token = takeOptional();
+    if (!token.has_value()) {
+        throw parser_exception("Unexpected EOF");
+    }
+    return token.value();
+}
+
+Program ParserImpl::parseProgram()
+{
+    std::vector<std::shared_ptr<Element>> elements;
+    while (!atEOF()) {
+        elements.emplace_back(parseElement());
+    }
+
+    return elements;
+}
+
+Token ParserImpl::eat(TokenType token_type)
+{
+    auto token = takeNext();
+    if (token.type() != token_type) {
+        throw parser_exception(std::string("Unexpected token (eat): ") + token.value());
+    }
+    return token;
 }
 
 // =====        Element Parsers         =====
 // ==========================================
 
-std::shared_ptr<Element> ParserImpl::parseElement() {
-  auto token = peekNext();
-  switch (token.type()) {
-    case tkIDENTIFIER:
-      return parseIdentifier();
-    case tkLPAREN:
-      return parseList();
-    case tkQUOTEMARK:
-      return parseQuotedElement();
-    default:
-      return parseLiteral();
-  }
+std::shared_ptr<Element> ParserImpl::parseElement()
+{
+    auto token = peekNext();
+    switch (token.type()) {
+        case tkIDENTIFIER:
+            return parseIdentifier();
+        case tkLPAREN:
+            return parseList();
+        case tkQUOTEMARK:
+            return parseQuotedElement();
+        default:
+            return parseLiteral();
+    }
 }
 
-std::shared_ptr<Identifier> ParserImpl::parseIdentifier() {
-  auto token_value = eat(tkIDENTIFIER).value();
-  return std::make_shared<Identifier>(token_value);
+std::shared_ptr<Identifier> ParserImpl::parseIdentifier()
+{
+    auto token_value = eat(tkIDENTIFIER).value();
+    return std::make_shared<Identifier>(token_value);
 }
 
-std::shared_ptr<List> ParserImpl::parseList() {
-  std::vector<std::shared_ptr<Element>> elements;
+std::shared_ptr<List> ParserImpl::parseList()
+{
+    std::vector<std::shared_ptr<Element>> elements;
 
-  for (auto tok = peekNext(); tok.type() != tkRPAREN; tok = peekNext()) {
-    elements.emplace_back(parseElement());
-  }
+    for (auto tok = peekNext(); tok.type() != tkRPAREN; tok = peekNext()) {
+        elements.emplace_back(parseElement());
+    }
 
-  return std::make_shared<List>(std::move(elements));
+    return std::make_shared<List>(std::move(elements));
 }
 
-std::shared_ptr<List> ParserImpl::parseQuotedElement() {
-  eat(tkQUOTEMARK);
-  auto element = parseElement();
-  auto list_wrapper = List([ Identifier("quote"), element ]);
-  return std::make_shared(list_wrapper);
+std::shared_ptr<List> ParserImpl::parseQuotedElement()
+{
+    eat(tkQUOTEMARK);
+    auto element    = parseElement();
+    auto quote_node = std::make_shared<Identifier>("quote");
+    return std::make_shared<List>(std::vector<std::shared_ptr<Element>>({quote_node, element}));
 }
 
 // =====        Literal Parsers         =====
 // ==========================================
 
 
-std::shared_ptr<Literal> ParserImpl::parseLiteral() {
-  auto token = peekNext();
-  switch (token.type()) {
-    case tkINTEGER:
-      return parseInteger();
-    case tkREAL:
-      return parseReal();
-    case tkBOOLEAN:
-      return parseBoolean();
-    case tkNULL:
-      return parseNull();
-  }
+std::shared_ptr<Literal> ParserImpl::parseLiteral()
+{
+    auto token = peekNext();
+    switch (token.type()) {
+        case tkINTEGER:
+            return parseInteger();
+        case tkREAL:
+            return parseReal();
+        case tkBOOLEAN:
+            return parseBoolean();
+        case tkNULL:
+            return parseNull();
+        default:
+            throw std::runtime_error("Unexpected token parse literal");
+    }
 }
 
-std::shared_ptr<Integer> ParserImpl::parseInteger() {
-  auto token_value = eat(tkINTEGER).value();
-  return std::make_shared<Integer>(std::stoi(token_value));
+std::shared_ptr<Integer> ParserImpl::parseInteger()
+{
+    auto token_value = eat(tkINTEGER).value();
+    return std::make_shared<Integer>(std::stoi(token_value));
 }
 
-std::shared_ptr<Real> ParserImpl::parseReal() {
-  auto token_value = eat(tkREAL).value();
-  return std::make_shared<Real>(std::stod(token_value));
+std::shared_ptr<Real> ParserImpl::parseReal()
+{
+    auto token_value = eat(tkREAL).value();
+    return std::make_shared<Real>(std::stod(token_value));
 }
 
-std::shared_ptr<Boolean> ParserImpl::parseBoolean() {
-  auto token_value = eat(tkBOOLEAN).value();
-  return ...;
+std::shared_ptr<Boolean> ParserImpl::parseBoolean()
+{
+    auto token_value = eat(tkBOOLEAN).value();
+    return std::make_shared<Boolean>(token_value == "true" ? true : false);
 }
 
-std::shared_ptr<Boolean> ParserImpl::parseNull() {
-  eat(tkNULL);
-  return FNULL;
+std::shared_ptr<Null> ParserImpl::parseNull()
+{
+    eat(tkNULL);
+    return std::make_shared<Null>();
 }
 
-}  // namespace flang
+} // namespace flang
--- src/flang/parse/parser_impl.hpp
+++ src/flang/parse/parser_impl.hpp
@@ -7,88 +7,94 @@
 #include "flang/parse/ast.hpp"
 #include "flang/tokenize/token.hpp"
 
-namespace flang {
-class ParserImpl {
- public:
-  explicit ParserImpl(std::vector<Token> const& tokens) : tokens_(tokens), next_index_(0) {}
+namespace flang
+{
+class ParserImpl
+{
+public:
+    explicit ParserImpl(std::vector<Token> const& tokens)
+        : tokens_(tokens)
+        , next_index_(0)
+    {
+    }
 
-  std::unique_ptr<ProgramNode> parseProgram();
+    Program parseProgram();
 
- private:
-  std::vector<Token> tokens_;
-  size_t next_index_;
+private:
+    std::vector<Token> tokens_;
+    size_t next_index_;
 
-  // ----- Primitive parsers -----
+    // ----- Primitive parsers -----
 
-  bool atEOF() const;
+    bool atEOF() const;
 
-  std::optional<Token> peekOptional() const;
+    std::optional<Token> peekOptional() const;
 
-  Token peekNext() const;
+    Token peekNext() const;
 
-  std::optional<Token> takeOptional();
+    std::optional<Token> takeOptional();
 
-  Token takeNext();
+    Token takeNext();
 
-  Token eat(TokenType token_type);
+    Token eat(TokenType token_type);
 
-  // =====        Element Parsers         =====
-  // ==========================================
+    // =====        Element Parsers         =====
+    // ==========================================
 
-  /**
-  Element :=
-    | Identifier
-    | List
-    | QuotedElement  <--- is desugared into List
-    | Literal
-  */
-  std::shared_ptr<Element> parseElement();
+    /**
+    Element :=
+      | Identifier
+      | List
+      | QuotedElement  <--- is desugared into List
+      | Literal
+    */
+    std::shared_ptr<Element> parseElement();
 
-  /**
-  Identifier := tkIDENTIFIER.
-  */
-  std::shared_ptr<Identifier> parseIdentifier();
+    /**
+    Identifier := tkIDENTIFIER.
+    */
+    std::shared_ptr<Identifier> parseIdentifier();
 
-  /**
-  List := ( Element* ) .
-  */
-  std::unique_ptr<List> parseList();
+    /**
+    List := ( Element* ) .
+    */
+    std::shared_ptr<List> parseList();
 
-  /**
-  Quote := ' Element.
-  */
-  std::unique_ptr<QuoteNode> parseQuotedElement();
+    /**
+    Quote := ' Element.
+    */
+    std::shared_ptr<List> parseQuotedElement();
 
-  // =====        Literal Parsers         =====
-  // ==========================================
+    // =====        Literal Parsers         =====
+    // ==========================================
 
-  /**
-  Literal :=
-    | Integer
-    | Real
-    | Boolean
-    | Null
-  */
-  std::shared_ptr<Literal> parseLiteral();
+    /**
+    Literal :=
+      | Integer
+      | Real
+      | Boolean
+      | Null
+    */
+    std::shared_ptr<Literal> parseLiteral();
 
-  /**
-  Integer := tkINTEGER.
-  */
-  std::shared_ptr<Integer> parseInteger();
+    /**
+    Integer := tkINTEGER.
+    */
+    std::shared_ptr<Integer> parseInteger();
 
-  /**
-  Real := tkREAL.
-  */
-  std::shared_ptr<Real> parseReal();
+    /**
+    Real := tkREAL.
+    */
+    std::shared_ptr<Real> parseReal();
 
-  /**
-  Boolean := tkBOOLEAN .
-  */
-  std::shared_ptr<Boolean> parseBoolean();
+    /**
+    Boolean := tkBOOLEAN .
+    */
+    std::shared_ptr<Boolean> parseBoolean();
 
-  /**
-  Null := tkNULL .
-  */
-  std::shared_ptr<CallNode> parseNull();
+    /**
+    Null := tkNULL
+    */
+    std::shared_ptr<Null> parseNull();
 };
-}  // namespace flang
+} // namespace flang
