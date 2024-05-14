8320134ccb0560a8ef3cf3f0ff640ecc318741ac
a.salyu <arthur.salio@gmail.com>
2024-05-14 16:41:29 +0300

    feat: tokenizer cpp

--- src/flang/tokenize/token.cpp
+++ src/flang/tokenize/token.cpp
@@ -1,14 +1,31 @@
 #include "flang/tokenize/token.hpp"
 
-namespace flang {
+namespace flang
+{
 Token::Token(TokenType type, std::string value, size_t line, size_t col)
-    : type_(type), value_(std::move(value)), location_({.line = line, .col = col}) {}
+    : type_(type)
+    , value_(std::move(value))
+    , location_({.line = line, .col = col})
+{
+}
 
-std::string const& Token::value() const { return value_; }
+std::string const& Token::value() const
+{
+    return value_;
+}
 
-std::string Token::value() { return value_; }
+std::string Token::value()
+{
+    return value_;
+}
 
-TokenType Token::type() const { return type_; }
+TokenType Token::type() const
+{
+    return type_;
+}
 
-Token::TokenLocation Token::location() const { return location_; }
-}  // namespace flang
+Token::TokenLocation Token::location() const
+{
+    return location_;
+}
+} // namespace flang
