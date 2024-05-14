2af66c0d62fc4f4f4b043eae3f06ddbbed8cfa5c
Furetur <furetur@gmail.com>
2024-05-14 23:15:24 +0300

    Implement EvalVisitor (except visitList)

--- include/flang/eval/eval_visitor.hpp
+++ include/flang/eval/eval_visitor.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include "environment_stack.hpp"
+#include <flang/parse/ast.hpp>
+#include <memory>
+#include <string>
 
 namespace flang
 {
 class EvalVisitor : public Visitor
 {
 public:
+    std::shared_ptr<Element> evalElement(std::shared_ptr<Element> node);
     void visitIdentifier(std::shared_ptr<Identifier> node) override;
     void visitInteger(std::shared_ptr<Integer> node) override;
     void visitReal(std::shared_ptr<Real> node) override;
@@ -14,6 +18,10 @@ public:
     void visitNull(std::shared_ptr<Null> node) override;
     void visitList(std::shared_ptr<List> node) override;
     void visitFunction(std::shared_ptr<Function> node) override;
+    void setResult(std::shared_ptr<Element> element);
+    std::shared_ptr<Element> loadVariable(std::string name);
+    void storeVariable(std::string name, std::shared_ptr<Element> element);
+    void throwRuntimeError(std::string message);
 
 private:
     EnvironmentStack env_;
