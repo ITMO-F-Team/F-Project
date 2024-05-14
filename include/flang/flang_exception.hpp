00a68eaa24fb189fac99c48736603d9b0add22ec
Furetur <furetur@gmail.com>
2024-05-14 21:17:00 +0300

    Add EnvironmentStack

--- include/flang/flang_exception.hpp
+++ include/flang/flang_exception.hpp
@@ -3,19 +3,42 @@
 #include <stdexcept>
 #include <string>
 
-namespace flang {
-class flang_exception : public std::runtime_error {
- public:
-  explicit flang_exception(std::string const& e_msg) : std::runtime_error(e_msg) {}
+namespace flang
+{
+class flang_exception : public std::runtime_error
+{
+public:
+    explicit flang_exception(std::string const& e_msg)
+        : std::runtime_error(e_msg)
+    {
+    }
 };
 
-class tokenizer_exception : public flang_exception {
- public:
-  explicit tokenizer_exception(std::string const& e_msg) : flang_exception(e_msg) {}
+
+class flang_runtime_error : public flang_exception
+{
+public:
+    explicit flang_runtime_error(std::string const& e_msg)
+        : flang_exception(e_msg)
+    {
+    }
 };
 
-class parser_exception : public flang_exception {
- public:
-  explicit parser_exception(std::string const& e_msg) : flang_exception(e_msg) {}
+class tokenizer_exception : public flang_exception
+{
+public:
+    explicit tokenizer_exception(std::string const& e_msg)
+        : flang_exception(e_msg)
+    {
+    }
 };
-}  // namespace flang
+
+class parser_exception : public flang_exception
+{
+public:
+    explicit parser_exception(std::string const& e_msg)
+        : flang_exception(e_msg)
+    {
+    }
+};
+} // namespace flang
---
+++ src/flang/eval/environment_stack.cpp
@@ -0,0 +1,51 @@
+
+#include <flang/flang_exception.hpp>
+#include <flang/parse/ast.hpp>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+
+#include "flang/eval/environment_stack.h"
+
+namespace flang
+{
+
+void EnvironmentStack::pushEnvironment()
+{
+    if (environment_stack_.size() == MAX_STACK_SIZE) {
+        throwRuntimeError("Stack overflow!");
+    }
+    Environment empty_scope;
+    environment_stack_.push_front(empty_scope);
+}
+
+void EnvironmentStack::popEnvironment()
+{
+    if (environment_stack_.size() == 1) {
+        throw std::runtime_error("Cannot pop global environment");
+    }
+    environment_stack_.pop_front();
+}
+
+std::shared_ptr<Element> EnvironmentStack::loadVariable(std::string name)
+{
+    for (auto const& env : environment_stack_) {
+        if (env.contains(name)) {
+            return env.at(name);
+        }
+    }
+    return nullptr;
+}
+
+void EnvironmentStack::storeVariable(std::string name, std::shared_ptr<Element> value)
+{
+    auto& env = environment_stack_.front();
+    env.insert_or_assign(name, value);
+}
+
+void EnvironmentStack::throwRuntimeError(std::string message)
+{
+    throw flang_runtime_error(message);
+}
+
+} // namespace flang
