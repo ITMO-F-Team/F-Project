454004b0bd5cc5ac764255c911ba4eccf9cd2043
Furetur <furetur@gmail.com>
2024-05-15 11:40:16 +0300

    ast: Replace Function with UserFunction and Builtin

--- include/flang/parse/ast.hpp
+++ include/flang/parse/ast.hpp
@@ -15,7 +15,8 @@ class Real;
 class Boolean;
 class Null;
 class List;
-class Function;
+class UserFunction;
+class Builtin;
 
 using Program = std::vector<std::shared_ptr<Element>>;
 
@@ -23,14 +24,17 @@ class Visitor
 {
 public:
     virtual void visitProgram(Program program);
-    virtual void visitIdentifier(std::shared_ptr<Identifier> node) = 0;
-    virtual void visitInteger(std::shared_ptr<Integer> node)       = 0;
-    virtual void visitReal(std::shared_ptr<Real> node)             = 0;
-    virtual void visitBoolean(std::shared_ptr<Boolean> node)       = 0;
-    virtual void visitNull(std::shared_ptr<Null> node)             = 0;
-    virtual void visitList(std::shared_ptr<List> node)             = 0;
-    virtual void visitFunction(std::shared_ptr<Function> node)     = 0;
+    virtual void visitIdentifier(std::shared_ptr<Identifier> node)     = 0;
+    virtual void visitInteger(std::shared_ptr<Integer> node)           = 0;
+    virtual void visitReal(std::shared_ptr<Real> node)                 = 0;
+    virtual void visitBoolean(std::shared_ptr<Boolean> node)           = 0;
+    virtual void visitNull(std::shared_ptr<Null> node)                 = 0;
+    virtual void visitList(std::shared_ptr<List> node)                 = 0;
+    virtual void visitUserFunction(std::shared_ptr<UserFunction> node) = 0;
+    virtual void visitBuiltin(std::shared_ptr<Builtin> node)           = 0;
 };
+
+class EvalVisitor;
 
 class Element
 {
@@ -173,34 +177,65 @@ private:
     std::vector<std::shared_ptr<Element>> elements_;
 };
 
-class Function final : public Element, std::enable_shared_from_this<Function>
+class UserFunction final : public Element, std::enable_shared_from_this<UserFunction>
 {
 public:
-    Function(std::shared_ptr<Identifier> identifier, std::function<void(Visitor&, std::vector<std::shared_ptr<Element>>)> impl)
-        : identifier_(std::move(identifier))
-        , impl_(impl)
+    UserFunction(std::string name, std::vector<std::string> formal_args, std::shared_ptr<Element> body)
+        : name_(name)
+        , formal_args_(formal_args)
+        , body_(body)
     {
     }
 
-    std::shared_ptr<Identifier> getName() const
+    std::string getName() const
     {
-        return identifier_;
+        return name_;
     }
 
-    template <class... Args>
-    void call(Args&&... args)
+    std::vector<std::string> const& getFormalArgs() const
     {
-        impl_(std::forward<Args>(args)...);
+        return formal_args_;
+    }
+
+    std::shared_ptr<Element> getBody() const
+    {
+        return body_;
     }
 
     void accept(Visitor& visitor) override
     {
-        visitor.visitFunction(shared_from_this());
+        visitor.visitUserFunction(shared_from_this());
     }
 
 private:
-    std::shared_ptr<Identifier> identifier_;
-    std::function<void(Visitor&, std::vector<std::shared_ptr<Element>>)> impl_;
+    std::string name_;
+    std::vector<std::string> formal_args_;
+    std::shared_ptr<Element> body_;
 };
+
+class Builtin final : public Element, std::enable_shared_from_this<Builtin>
+{
+public:
+    explicit Builtin(std::string name, int arity)
+        : name_(name)
+        , arity_(arity)
+    {
+    }
+
+    std::string getName() const
+    {
+        return name_;
+    }
+
+    void accept(Visitor& visitor) override
+    {
+        visitor.visitBuiltin(shared_from_this());
+    }
+
+private:
+    std::string name_;
+    int arity_;
+};
+
 
 } // namespace flang
